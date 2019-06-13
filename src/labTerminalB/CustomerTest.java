85a8d69029e24ec39a8e32f6247ac649cbef51c1
SyedMazharHussain <48942260+SyedMazharHussain@users.noreply.github.com>
2019-06-13 11:24:47 +0500

    Update CustomerTest.java

--- src/labTerminalB/CustomerTest.java
+++ src/labTerminalB/CustomerTest.java
@@ -1,36 +1,33 @@
-package labTerminalB;
+package labterminalB;
 
-import org.junit.Test;
+
 
 public class CustomerTest {
-	Movie m1 = new Movie("Spancer", 0);
-	Movie m2 = new Movie("Speed", 1);
-	Customer me = new Customer("Nadir");
-	Customer me2 = new Customer("Allah Ditta");
-	Rental r1 = new Rental(m1, 23);
-	Rental r2 = new Rental(m2,4);
-	
-	/**
-	 * Test 1 Customer rented two movies one REGULAR and one NEW_RELEASE
-	 */
+
 	@Test
-	public void testStatement1() {
-		me.addRental(r1);
-		me.addRental(r2);
-		//fail("Not yet implemented");
-		String output1 = "Rental Record for Nadir\r\n" + 
-				"	Spancer	33.5\r\n" + 
-				"	Speed	12.0";
-		if(output1.equals(me.statement()))
-		{
-			assert true;
+	public void test() {
+		Movie m1 = new Movie("AliBaba", 0);
+		Movie m2 = new Movie("NeedForSpeed", 1);
+		Customer me = new Customer("Saood");
+		Customer me2 = new Customer("ahsan");
+		Rental r1 = new Rental(m1, 20);
+		Rental r2 = new Rental(m2,20);
+		
+		/**
+		 * Test 1 Customer rented two movies one REGULAR and one NEW_RELEASE
+		 */
+		
+			me.addRental(r1);
+			me.addRental(r2);
+			//fail("Not yet implemented");
+			String output1 = "Rental Record for Saood\r\n" + 
+					"	AliBaba	29.0\r\n" + 
+					"	Speed	60.0";
+			if(output1.equals(me.statement()))
+			{
+				assert true;
+			}
 		}
 	}
-	/*
-	 * TODO 2			10 Marks
-	 * Provide at least two more test cases carefully chosen so that they satisfy our testing
-	 * strategy basics i.e. input partition etc
-	 */
-	
 
-}
+
